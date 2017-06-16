4ee5dc0d82b861f2ba413f638a79993e8d4935c2
zhouzhiyong1 <zhouzhiyong_work@163.com>
2017-06-16 09:11:29 +0800

    Initial Commit

---
+++ ShopCarDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ShopCarDemo
+//
+//  Created by zhouzhiyong on 2017/6/16.
+//  Copyright © 2017年 bocweb. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
